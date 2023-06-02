000db6b7e8fc316ca6b2b3677440227e54767489
Jaikishore <jaikishore@dlearners.in>
2023-06-02 19:51:21 +0530

    Script added

---
+++ DummyScript1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DummyScript1 : MonoBehaviour
+{
+    public Image image;
+    public void ColourChangeOnClick1(){
+        image.color = Color.yellow;
+    }
+}
